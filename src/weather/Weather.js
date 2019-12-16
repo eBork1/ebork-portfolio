5732db92fe4545e6b7b3b428db12104c0f8e778a
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-16 18:46:12 -0500

    create weather-app component

---
+++ src/weather/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render(){
+        return(
+            <div className="text-light">
+                Weather App
+            </div>
+        )
+    }
+}
+
+export default Weather;
