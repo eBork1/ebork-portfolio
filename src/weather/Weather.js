93b55f9ee21cc84a08053272746c411fd8843e78
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-16 19:09:25 -0500

    install axios

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class Weather extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+            userZip: "40515",
+        }
+        this.getWeather = this.getWeather.bind(this);
+    }
+    
+    getWeather(){
+        const KEY = "2510cc4345ae2fb5c4f9c8f0053faa8c";
+        axios({
+            method: 'get',
+            url: "https://api.openweathermap.org/data/2.5/weather?zip=" + this.state.userZip + "&appid=" + KEY,
+        })
+        .then(response => {
+             console.log(response.data);
+        });
+    }
+
+     componentDidMount(){
+         this.getWeather();
+     }
+
     render(){
         return(
             <div className="text-light">
