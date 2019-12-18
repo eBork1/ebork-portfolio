61125900f5c453a07acc272aba5497d7e9922a72
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-18 01:13:48 -0500

    create home button

---
+++ src/HomeBtn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomeBtn extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <a href="/"> 
+                <button className="btn btn-link">Home</button>
+                </a>
+            </div>
+        )
+    }
+}
+export default HomeBtn;
