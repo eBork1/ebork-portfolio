7c686f3b219c920d008eccb8da8c6f8a422799aa
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-18 01:15:24 -0500

    working countdown timer and reset button

--- src/countdown-timer/CountdownTimer.js
+++ src/countdown-timer/CountdownTimer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HomeBtn from '../HomeBtn';
 
 class CountdownTimer extends React.Component {
 
@@ -6,24 +7,23 @@ class CountdownTimer extends React.Component {
         super(props);
         this.state = {
             currentDate: '', // Current local date
-            endDateFull: '', // Full end date + time
             endDate: '', // End month, day, year
             endTime: '', // End time, hour, min, sec
-            timeLeft: '',
-            sec: '',
-            min: '',
-            hour: '',
-            day: '',
+            sec_remaining: '',
+            min_remaining: '',
+            hours_remaining: '',
+            days_remaining: '',
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this);
         this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.resetTime = this.resetTime.bind(this);
     }
 
     // Set the interval to tick every sec
     componentDidMount() {
         this.interval = setInterval(() => this.tick(), 1000);
-        
+
     }
 
     componentWillUnmount() {
@@ -32,10 +32,8 @@ class CountdownTimer extends React.Component {
 
     // Get current date, store in state
     tick() {
-        const date = new Date().toLocaleString();
-        // console.log(date);
+        const date = new Date().getTime();
         this.setState({ currentDate: date });
-        // console.log(this.state);
     }
 
     handleDateChange(event) {
@@ -47,36 +45,91 @@ class CountdownTimer extends React.Component {
     }
 
     handleSubmit(event) {
-        console.log(this.state.endDate);
-        console.log(this.state.endTime);
-        console.log("current time: " + this.state.currentDate);
         event.preventDefault();
-        this.calculateTimeLeft();
+        setInterval(() => {
+            this.calculateTimeLeft();
+        }, 1000);
+        console.log(this.state);
     }
 
-    calculateTimeLeft(){
-        const END_DATE = this.state.endDate + ", " + this.state.endTime;
-        console.log("end date: " + END_DATE);
-        
-        const timeLeft = END_DATE - this.state.currentDate;
-        console.log(JSON.stringify(timeLeft));
+    calculateTimeLeft() {
+        // Got the users end date+time and combined them into the JS Date() format
+        // Converted the combined date and time to milliseconds so it can be compared with the current date (also in milliseconds)
+        // Calculated time left and stored it in State
+        const COMBINED_DATE = this.state.endDate + "T" + this.state.endTime + ":00";
+        const END_DATE = new Date(COMBINED_DATE).getTime();
+        const TIME_LEFT = END_DATE - this.state.currentDate;
+
+        // Converted the milliseconds back to individual variables for min/sec/hour day/month/year
+        const DAYS = Math.floor(TIME_LEFT / (1000 * 60 * 60 * 24));
+        const HOURS = Math.floor((TIME_LEFT % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const MINUTES = Math.floor((TIME_LEFT % (1000 * 60 * 60)) / (1000 * 60));
+        const SECONDS = Math.floor((TIME_LEFT % (1000 * 60)) / 1000);
+
+
+        this.setState({
+            days_remaining: DAYS,
+            hours_remaining: HOURS < 10 ? "0" + HOURS : HOURS,
+            min_remaining: MINUTES < 10 ? "0" + MINUTES : MINUTES,
+            sec_remaining: SECONDS < 10 ? "0" + SECONDS : SECONDS
+        });
+
+    }
+
+    resetTime() {
+        this.setState({
+            endDate: '',
+            endTime: '',
+            days_remaining: '',
+            hours_remaining: '',
+            min_remaining: '',
+            sec_remaining: '',
+        });
+        window.location.reload(false);
     }
 
     render() {
         // console.log(this.state);
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-lg-5">
-                        <p className="display-4 text-white">End Date</p>
-                        <form onSubmit={this.handleSubmit}>
-                            <input className="m-2" type="date" name="date" value={this.state.endDate} onChange={this.handleDateChange} /> <br />
-                            <input className="m-2" type="time" name="time" value={this.state.endTime} onChange={this.handleTimeChange} /> <br />
-                            <input type="submit" className="btn btn-block" />
-                        </form>
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-1">
+                            <HomeBtn />
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-lg-5">
+                            <p className="display-4 text-white">End Date</p>
+                            <form onSubmit={this.handleSubmit}>
+                                <input className="m-2" type="date" name="date" value={this.state.endDate} onChange={this.handleDateChange} /> <br />
+                                <input className="m-2" type="time" name="time" value={this.state.endTime} onChange={this.handleTimeChange} /> <br />
+                                <input type="submit" className="btn btn-block" />
+                            </form>
+                            <br />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <p className="display-3 text-white">
+                                {this.state.days_remaining ? this.state.days_remaining + " days" : null}
+                            </p><br />
+                            <p className="display-4 text-white">
+                                {this.state.hours_remaining ? this.state.hours_remaining + ":" : null}
+                                {this.state.min_remaining ? this.state.min_remaining + ":" : null}
+                                {this.state.sec_remaining ? this.state.sec_remaining : null}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12 col-lg-5">
+                            <button className="btn btn-block fixed-bottom mb-2" onClick={this.resetTime}>Reset</button>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
