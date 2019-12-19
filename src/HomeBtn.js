8bc684a0da50ae7b62ade7d896386bf71b4a2b71
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-18 20:33:23 -0500

    made updates to home button, fixed icon size

--- src/HomeBtn.js
+++ src/HomeBtn.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 class HomeBtn extends React.Component {
 
     render() {
         return (
-            <div>
-                <a href="/"> 
-                <button className="btn btn-link">Home</button>
-                </a>
+            <div className="containter">
+                <div className="row">
+                    <div className="col-3 mt-3">
+                        <a href="/" className="shadow-lg">
+                            <FontAwesomeIcon
+                                icon={faArrowLeft}
+                                color="#c9c9c9"
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faHome}
+                                color="#c9c9c9"
+                                size="2x"
+                            />
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
