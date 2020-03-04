691dd512c0d78b002d5558c616f7458d625abcd8
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-03 20:53:22 -0500

    update and complete basic functional tic tac toe game with stateful components

--- src/tic-tac-toe/TicTacToe.js
+++ src/tic-tac-toe/TicTacToe.js
@@ -1,26 +1,113 @@
 import React from 'react';
+import './tictactoe.css';
 
-class TicTacToe extends React.Component {
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={() => props.onClick()}
+        >
+            {props.value}
+        </button>
+    );
+}
 
-    constructor(props){
+class Board extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            wins: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]],
-            stepNumber: 0,
+        this.state = {
+            squares: Array(9).fill(null),
             xIsNext: true,
         }
     }
 
-    drawGameBoard(){
-
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
-    render(){
-        return(
-            <div>
-                TicTacToe
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        }
+        return (
+            <div className="container mt-5">
+                <div className="status text-white">{status}</div>
+                <div className="row ml-3">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="row ml-3">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="row ml-3">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+                <div className="row ml-3">
+                    <button className="btn btn-danger" onClick={() => window.location.reload(false)}>
+                        Reset
+                    </button>
+                </div>
             </div>
-        )
+        );
     }
 }
-export default TicTacToe;
+
+export default class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    //
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
