e36d3c9e9a912e2933bfaebc45294d44c36bd64c
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-03-05 01:56:11 -0500

    finish main functionality of game

--- src/tic-tac-toe/TicTacToe.js
+++ src/tic-tac-toe/TicTacToe.js
@@ -87,7 +87,6 @@ export default class Game extends React.Component {
                     <Board />
                 </div>
                 <div className="game-info">
-                    //
                 </div>
             </div>
         );
